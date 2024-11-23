3e1838bf41c1ff246b8d563a0860d2689ad1fc03
treasure-sky <ych0486666@gmail.com>
2024-11-23 21:17:07 +0900

    feat: alloc_SRC_and_DEST 구현

--- libLab2.c
+++ libLab2.c
@@ -19,21 +19,39 @@ int alloc_SRC_and_DEST(char **SRC, char **DEST, size_t file_size)
 {
     int ret = 0;
 
-    /*
-     *  write your code
-     */
-    return ret;
+    *SRC = malloc(file_size);
+    if (*SRC == NULL)
+    {
+        perror("SRC malloc 실패");
+        ret = -1;
+        return ret;
+    }
+
+    *DEST = malloc(file_size);
+    if (*DEST == NULL)
+    {
+        perror("DEST malloc 실패");
+        free(*SRC); // SRC는 이미 할당된 상태이므로 이를 해제해줌.
+        ret = -1;
+        return ret;
+    }
+
+    return 0;
 }
 
 size_t getSize(int fd)
 {
+    size_t file_size = 0;
+
     struct stat st;
     if (fstat(fd, &st) == -1)
     {
         perror("fstat 실패");
         return -1;
     }
-    return (size_t)st.st_size;
+
+    file_size = (size_t)st.st_size;
+    return file_size;
 }
 
 void movs_memcpy(char *DEST, char *SRC, size_t size)
