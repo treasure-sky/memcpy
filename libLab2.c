15d6f929e5e36f966808e22e73b758263ccc6390
treasure-sky <ych0486666@gmail.com>
2024-11-23 20:09:43 +0900

    refactor: 포인터 위치 수정

--- libLab2.c
+++ libLab2.c
@@ -7,8 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-void old_memcpy(char* DEST, char* SRC, size_t size)
+void old_memcpy(char *DEST, char *SRC, size_t size)
 {
     char *d = DEST;
     const char *s = SRC;
@@ -16,44 +15,37 @@ void old_memcpy(char* DEST, char* SRC, size_t size)
         *d++ = *s++;
 }
 
-
-
-int alloc_SRC_and_DEST(char** SRC, char** DEST,size_t file_size)
+int alloc_SRC_and_DEST(char **SRC, char **DEST, size_t file_size)
 {
-    int ret=0;
+    int ret = 0;
 
     /*
      *  write your code
      */
     return ret;
-
 }
 
 size_t getSize(int fd)
 {
-    size_t file_size=0;
-    /*
-     *  write your code
-     */
-    return file_size;
+    struct stat st;
+    if (fstat(fd, &st) == -1)
+    {
+        perror("fstat 실패");
+        return -1;
+    }
+    return (size_t)st.st_size;
 }
 
-void movs_memcpy(char* DEST, char* SRC, size_t size)
+void movs_memcpy(char *DEST, char *SRC, size_t size)
 {
     /*
      *  write your code
      */
 }
 
-
-
-
-void custom_memcpy(char* DEST, char* SRC, size_t size)
+void custom_memcpy(char *DEST, char *SRC, size_t size)
 {
     /*
      *  write your code
      */
 }
-
-
-
